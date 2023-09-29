3de7feab390a83d2cedc9bb6d04f20d01049f0f1
Alya Kameela <alyakameela@Alyas-MacBook-Air.local>
2023-09-29 17:54:29 +0700

    first commit

---
+++ EmojiPicker/EmojiPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerApp.swift
+//  EmojiPicker
+//
+//  Created by Alya Kameela on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
